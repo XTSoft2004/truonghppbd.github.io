c43672bcc3eb01cb2a6528e65bc1a8c64217ca81
tranxuantruong420 <tranxuantruong420@gmail.com>
2023-04-26 10:18:19 +0700

    fix js

---
+++ java.js
@@ -0,0 +1,12 @@
+setTimeout(function () {
+  alert("Này cậu kia");
+}, 2000);
+setTimeout(function () {
+  alert("Đúng chính là cậu đó, biết hôm nay là ngày gì khom");
+}, 5000);
+setTimeout(function () {
+  alert("Cậu có biết cậu rất đẹp trai không , waiting...");
+}, 10000);
+setTimeout(function () {
+  alert("Chúc mừng sinh sinh nhật 26/4 nha");
+}, 17000);
